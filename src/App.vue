6a2d62664c579aab984e0a9757b12172e69d7850
rukiye <rukiyeaydinlive@gmail.com>
2024-11-18 11:09:29 +0300

    reaction timer game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">PLAY</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    Results
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      // console.log(this.delay);
+      this.showResults = false;
+      
+    },
+    endGame(reactionTime){
+      this.score = reactionTime,
+      this.isPlaying = false;
+      this.showResults = true;
+    }
   }
 }
 </script>
@@ -22,5 +48,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  padding: 10px 20px;
+  background-color: #45b66e;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+button:disabled{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,56 @@
+<template>
+    <p>Reaction time: {{ score }} ms</p>
+    <p :class="rankClass">{{ rank }}</p>
+</template>
+
+<script>
+export default{
+    props: ['score'],
+    data(){
+        return{
+            rank: null,   
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = "Godlike";
+        } else if(this.score < 400){
+            this.rank = "Rapid Reflexes";
+        } else if(this.score < 600){
+            this.rank = "Average";
+        } else {
+            this.rank = "Snail Pace..";
+        }
+    },
+    computed: {
+        rankClass() {
+            if (this.score < 250) {
+                return "godlike";
+            } else if (this.score < 400) {
+                return "rapid-reflexes";
+            } else if (this.score < 600) {
+                return "average";
+            } else {
+                return "snail-pace";
+            }
+        },
+    },
+}
+</script>
+
+<style scoped>
+ 
+.godlike{
+    color: gold;
+}
+.rapid-reflexes{
+    color: green;
+}
+.average{
+    color: blue;
+}
+.snail-pace{
+    color: red;
+}
+
+</style>
